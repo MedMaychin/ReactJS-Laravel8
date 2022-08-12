94f2ec37e8175b890c5565199e0669a57edae273
MAYCHINE MOHAMED <31590745+MedMaychin@users.noreply.github.com>
2022-08-12 17:42:59 +0100

    c
    backend

--- frontend/src/pages/Addstudent.js
+++ frontend/src/pages/Addstudent.js
@@ -1,39 +1,64 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-function Addstudent() {
-    return (
-        <div className='container '>
-            <div className='row'>
-                <div className='col-md-6'>
-                    <div className='card'>
-                        <div className='card-header'>
-                            <h4>Add Student Data
-                                <Link to={'/'} className='btn btn-warning btn-sm float-end'>Back</Link>
-                            </h4>
-                        </div>
-                        <div className='card-body'>
-                            <div class="form-group mb-3">
-                              <label>Name</label>
-                              <input type="text" name="name" value={this.state.name} className="form-control" placeholder="Student name" onChange={this.handleInput}/>
+export class Addstudent extends Component {
+
+
+    state = {
+        name: '',
+        class: '',
+        phone: ''
+    }
+
+
+    handleInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    saveStudent = async (e) => {
+        e.preventDefault();
+
+        const res = await axios.post('/api/add-student', this.state);
+    }
+    render() {
+        return (
+            <div className='container '>
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <div className='card'>
+                            <div className='card-header'>
+                                <h4>Add Student Data
+                                    <Link to={'/'} className='btn btn-warning btn-sm float-end'>Back</Link>
+                                </h4>
                             </div>
-                            <div class="form-group mb-3">
-                              <label>Class</label>
-                              <input type="text" name="class" value={this.state.class} className="form-control" placeholder="Student Class" onChange={this.handleInput}/>
-                            </div>
-                            <div class="form-group mb-3">
-                              <label>Phone</label>
-                              <input type="text" name="phone" value={this.state.phone} className="form-control" placeholder="Phone" onChange={this.handleInput}/>
-                            </div>
-                            <div class="form-group mb-3">
-                                <button type="submit" class="btn btn-success">Save</button>
+                            <div className='card-body'>
+                                <form onSubmit={saveStudent}>
+                                    <div class="form-group mb-3">
+                                        <label>Name</label>
+                                        <input type="text" name="name" value={this.state.name} className="form-control" placeholder="Student name" onChange={this.handleInput} />
+                                    </div>
+                                    <div class="form-group mb-3">
+                                        <label>Class</label>
+                                        <input type="text" name="class" value={this.state.class} className="form-control" placeholder="Student Class" onChange={this.handleInput} />
+                                    </div>
+                                    <div class="form-group mb-3">
+                                        <label>Phone</label>
+                                        <input type="text" name="phone" value={this.state.phone} className="form-control" placeholder="Phone" onChange={this.handleInput} />
+                                    </div>
+                                    <div class="form-group mb-3">
+                                        <button type="submit" class="btn btn-success">Save</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Addstudent
